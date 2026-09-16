d0b1bad376e6ce8ee36a1c0bac733d4cec105cbb
khodakovskyvladislav <khodakovsky.vladislav@gmail.com>
2026-09-16 21:46:09 +0300

    fix tiny miss

--- app/test_main.py
+++ app/test_main.py
@@ -7,6 +7,7 @@ from app.main import get_human_age
     "cat_age, dog_age, expected",
     [
         (-1, 0, [0, 0]),
+        (0, -5, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
